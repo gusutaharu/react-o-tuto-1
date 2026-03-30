1ae80f9833011f62370453efe5e8859f5dd039ec
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2026-03-30 11:12:51 +0900

    インタラクティブなコンポーネントの作成

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,3 +1,13 @@
-export default function Square({value}) {
-  return <button className="square">{value}</button>;
+import { useState } from "react";
+
+export default function Square() {
+  const [ value, setValue ] = useState(" ");
+  function handleClick() {
+    setValue("X");
+  }
+  return (
+    <button className="square" onClick={handleClick}>
+      {value}
+    </button>
+  );
 }
